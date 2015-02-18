5b2d3bb06d5e88b1ba50e2aee91540709cfb7ed8
Xavier Robin <xavalias-github@xavier.robin.name>
2015-02-18 16:48:37 +0100

    Adding dummy sample data for testing and debugging

---
+++ R/ENSTest.R
@@ -0,0 +1,12 @@
+#' A dummy sample data to test the stuff
+#' @docType data
+#' @name ENSTest
+#' @examples
+#' data(ENSTest)
+#' \dontrun{
+#' # Refresh from the source file
+#' ENSTest <- read.csv(system.file("data/ENSTest.csv", package="MCMS", mustWork = TRUE))
+#' save(ENSTest, file="inst/data/ENSTest.csv")
+#' }
+#' 
+NULL
