f6709974bbd250c6e7e923de03c61e68cff35716
Xavier Robin <xavalias-github@xavier.robin.name>
2015-02-19 18:35:34 +0100

    Update doc for ENSTest as well

--- R/ENSTest.R
+++ R/ENSTest.R
@@ -6,6 +6,7 @@
 #' \dontrun{
 #' # Refresh from the source file
 #' ENSTest <- read.csv(system.file("data/ENSTest.csv", package="MCMS", mustWork = TRUE))
+#' ENSTest$reference <- as.character(ENSTest$reference)
 #' save(ENSTest, file="data/ENSTest.RData")
 #' }
 #' 
