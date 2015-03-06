8904a976b6e53a25aaebbc453f81683953673850
Xavier Robin <xavalias-github@xavier.robin.name>
2015-03-06 16:01:13 +0100

    Initial shot @ Monte Carlo class

---
+++ src/MonteCarlo.cpp
@@ -0,0 +1,48 @@
+//#include "MonteCarlo.hpp"
+//
+//void MonteCarlo::iterate() {
+//	// Choose parameter to change
+//	ParamSpecs& randomParam = paramsSpecs.getRandomElementByReference();
+//	if (randomParam.category == randomParam::ParamCategory::c) {
+//		// 1. resample a new c
+//		double newC = resample(c);
+//		// 2. Update the parameter
+//		double oldC = c.setC(randomParam.index1, newC);
+//		// 3. Query new likelihood
+//		// 4. Query new prior
+//		// 5 decide acceptance
+//		if (accept) {
+//			l.accept(randomParam.index1);
+//			prior.accept(randomParam.index1);
+//		}
+//		else {
+//			c.setC(randomParam.index1, oldC);
+//		}
+//
+//	}
+//	else { // we have an o
+//
+//	}
+//}
+//
+//
+//void MonteCarlo::iterate(unsigned int i) {
+//	while (i--) {
+//		iterate();
+//	}
+//}
+//
+//static ParamSpecsVector makeParamSpecsVector(const cParams& c, const oParams& o) {
+//	std::vector<ParamSpecs> paramSpecs;
+//	// Fill the paramSpecs vector with o and c
+//	for (size_t i = 0; i < c.size(); ++i) {
+//		paramSpecs.push_back(ParamSpecs(ParamSpecs::ParamCategory::c, i));
+//	}
+//	for (size_t i = 0; i < o.size(); ++i) {
+//		for (size_t j = 0; j < o.size(i); ++j) {
+//			paramSpecs.push_back(ParamSpecs(ParamSpecs::ParamCategory::o, i, j));
+//		}
+//	}
+//	return paramSpecs;
+//
+//}
---
+++ src/MonteCarlo.hpp
@@ -0,0 +1,56 @@
+//#pragma once
+//
+//#include "Likelihood.hpp"
+//#include "Priors.hpp"
+//
+//class ParamSpecs {
+//	public:
+//	enum ParamCategory { c, o };
+//	const ParamCategory category;
+//	const size_t index1, const index2; // 1: c or sample (o); 2, o only: site
+//
+//	ParamSpecs(const ParamCategory& aCategory, const size_t anIndex1): // c
+//		category(aCategory), index1(anIndex1) {}
+//	ParamSpecs(const ParamCategory& aCategory, const size_t anIndex1, const size_t anIndex2): // o
+//		category(aCategory), index1(anIndex1), index2(anIndex2) {}
+//};
+//
+///** A linear representation of all the parameters */
+//class ParamSpecsVector {
+//	std::mt19937_64& rng;
+//	std::uniform_int_distribution<size_t> dist;
+//	const std::vector<ParamSpecs> paramSpecs;
+//
+//	public:
+//	ParamSpecsVector(const std::vector<ParamSpecs>& someParamSpecs, std::mt19937_64& anRNG):
+//		rng(anRNG), dist(0, someParamSpecs.size()), paramSpecs(someParamSpecs) {}
+//
+//	ParamSpecs& getRandomParamSpecByReference() {
+//		return paramSpecs[dist(rng)];
+//	}
+//};
+//
+//class MonteCarlo {
+//	cParams c;
+//	oParams o;
+//	Prior p;
+//	Likelihood l;
+//	const ParamSpecsVector paramSpecs; // a vector with all the parameters
+//	std::mt19937_64& rng; // The random numbers for the whole MC run
+//
+//	static ParamSpecsVector makeParamSpecsVector(const cParams& c, const oParams& o);
+//
+//	public:
+//	MonteCarlo(const Likelihood& aLikelihood,
+//		const cParams& aC, const oParams& anO,
+//		std::mt19937_64& anRNG):
+//		c(aC),
+//		o(anO),
+//		p(aC, anO),
+//		l(aLikelihood),
+//		paramSpecs(makeParamSpecs(aC, anO), anRNG),
+//		rng(anRNG){}
+//
+//	void iterate(unsigned long);
+//	void iterate();
+//};
