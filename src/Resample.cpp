dab6c8275004d435951dcfed16e5597ae3d0c18b
Xavier Robin <xavalias-github@xavier.robin.name>
2015-03-09 10:40:57 +0100

    Minor fixes in Resampler

--- src/Resample.cpp
+++ src/Resample.cpp
@@ -21,18 +21,18 @@ inline double Resampler::resampleO(const ParamSpecs& paramSpec, const double par
 	}
 }
 
-double resampleCFromPrior(const ParamSpecs& paramSpec, const double param) {
+inline double resampleCFromPrior(const ParamSpecs& paramSpec, const double param) {
 	return paramSpec.prior.pdf(param);
 }
-double resampleOFromPrior(const ParamSpecs& paramSpec, const double param) {
+inline double resampleOFromPrior(const ParamSpecs& paramSpec, const double param) {
 	return paramSpec.prior.pdf(param);
 }
 
-double resampleCStandard(const ParamSpecs& paramSpec, const double param) {
+inline double resampleCStandard(const ParamSpecs& paramSpec, const double param) {
 	return param + c_normal(rng);
 }
 
-double resampleOStandard(const ParamSpecs& paramSpec, const double param) {
+inline double resampleOStandard(const ParamSpecs& paramSpec, const double param) {
 	double k = paramSpec.prior.pdf(param) * (1 / (2 * (1 - param)) - 1 / (2 * param)) * constants.k_scale;
 	boost::random::normal_distribution<double>::param_type local_params(0, k);
 	return o + o_normal(rng, local_params);
--- src/Resample.hpp
+++ src/Resample.hpp
@@ -4,21 +4,21 @@
 #include "Parameters.hpp"
 
 class Resampler {
-	//const Constants& constants;
+	const Constants& constants;
 	std::mt19937_64& rng;
 	const boost::random::uniform_int_distribution<size_t> unif01;
 	const boost::random::normal_distribution<double> c_normal;
 	const boost::random::normal_distribution<double> o_normal;
 	//const boost::random::normal_distribution<double>::param_type o_normal_params;
 
-	Resampler(std::mt19937_64& anRNG, const Constants& constants):
-		rng(aRND), unif01(0, 1),
-		c_normal(0, constants.c_sd), o_normal(0, constants.o_sd), o_normal_params(o_normal.params) {}
+	Resampler(std::mt19937_64& aRNG, const Constants& someConstants):
+		constants(someConstants), rng(aRNG),
+		unif01(0, 1), c_normal(0, constants.c_sd), o_normal(0, constants.o_sd) {}
 
 	public:
 	/** Randomly choose if we're doing a prior move */
 	bool priorMove() {
-		return unif01(rng) > (1 - constants.priorMoveProportion)
+		return unif01(rng) > (1 - constants.priorMoveProportion);
 	}
 
     double resampleC(const ParamSpecs& paramSpec, const double param);
