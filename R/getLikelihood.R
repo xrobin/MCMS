b2f966ecfb1c136f6ec1e5df5859600830b9a211
Xavier Robin <xavalias-github@xavier.robin.name>
2015-04-21 14:20:03 +0200

    More examples and debug output in getLikelihood

--- R/getLikelihood.R
+++ R/getLikelihood.R
@@ -1,14 +1,18 @@
 #' Compute the likelihood of a PeptidesModel
 #' @param PeptidesModel the PeptidesModel data
 #' @param var.model the variance model
-#' @param c, o values of the parameters for which to estimate the likelihood
+#' @param c,o values of the parameters for which to estimate the likelihood
 #' @param c.prior.sd,o.prior.shape1,o.prior.shape2 parameters for the prior
 #' @param verbose if TRUE, will print internal details of the Monte Carlo object (elements and pointers)
 #' @examples
-#' data(ENSTest)
+#' data(ENSTest, var.model)
 #' ENSTestProtein <- Protein(ENSTest)
 #' ENSTestModel <- PeptidesModel(ENSTestProtein)
 #' getLikelihood(ENSTestModel, var.model)
+#' # With custom c
+#' someC <- ENSTestModel@@c
+#' someC[] <- 0
+#' getLikelihood(ENSTestModel, var.model, c = someC)
 #' @export
 #' @useDynLib MCMS
 getLikelihood <- function(PeptidesModel, var.model, c = PeptidesModel@c, o = PeptidesModel@o,
