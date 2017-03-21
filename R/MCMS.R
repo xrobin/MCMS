5e0b1abe58acdf65f7eee9e6b3c54422a675ea3f
Xavier Robin <xavalias-github@xavier.robin.name>
2017-03-21 10:11:25 +0100

    Don't use a default seed in C++ - rely on R only

--- R/MCMS.R
+++ R/MCMS.R
@@ -29,10 +29,11 @@
 MCMS <- function(PeptidesModel, var.model, n, n.out, burn.in,
 				 c.prior.sd = sqrt(2), o.prior.shape1 = .5, o.prior.shape2 = .5,
 				 prior_move_proportion = .02, c_sd = 0.05,
-				 o_sd = 0.05, o_k_scale = 1/100, o_restrict = 1e-3, verbose = FALSE, cooling = TRUE,
-				 seed = round(runif(10, -.Machine$integer.max, .Machine$integer.max))) {
+				 o_sd = 0.05, o_k_scale = 1/100, o_restrict = 1e-3,
+				 seed = round(runif(10, -.Machine$integer.max, .Machine$integer.max)),
+				 verbose = FALSE, cooling = TRUE) {
 	run_MCMC_Cpp(PeptidesModel, var.model, n, n.out, burn.in, c_prior_sd = c.prior.sd, o_prior_shape1 = o.prior.shape1,
 				 o_prior_shape2 = o.prior.shape2, o_restrict = o_restrict,
 				 prior_move_proportion = prior_move_proportion, c_sd = c_sd, o_sd = o_sd, o_k_scale = o_k_scale,
-				 verbose = verbose, cooling = cooling, seed = seed)
+				 seed = seed, verbose = verbose, cooling = cooling)
 }
