eda7bea7f7570b6f9b12874dfca58d060ea9492e
Xavier Robin <xavalias-github@xavier.robin.name>
2018-01-16 17:12:37 +0100

    R CMD check complains about file encoding

--- R/dplyr_fix.R
+++ R/dplyr_fix.R
@@ -1,7 +1,7 @@
-## quiets concerns of R CMD check re non-standard evaluation 
+## quiets concerns of R CMD check re non-standard evaluation
 # Messages like:
 # make_unique_modifications: no visible binding for global variable
-# ‘start’
+# 'start'
 # Undefined global functions or variables:
 #   modifications pair ratio start
 # See https://stackoverflow.com/questions/9439256/ for possibly updated fixes:
