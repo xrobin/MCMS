015572dd18e13bf057cc98ceea2fea5e3c149c7c
Xavier Robin <xavalias-github@xavier.robin.name>
2018-01-16 15:56:46 +0100

    Do more in MCMS: default n + effectiveSize

--- R/MCMS.R
+++ R/MCMS.R
@@ -3,8 +3,9 @@
 #' @param Peptides the Peptides model data
 #' @param var.model the variance model
 #' @param n number of iterations
-#' @param n.out number of iterations to record
+#' @param n.out number of iterations to record. Should be a divisor of (1 - burn.in) * n for best results.
 #' @param burn.in the burn-in time (integer, NOT a proportion)
+#' @param max.repeats the maximum number of repeats of 1E9 iterations to run
 #' @param c.prior.sd,o.prior.shape1,o.prior.shape2 parameters for the prior
 #' @param prior_move_proportion the proportion of moves that are sampled from the prior
 #' @param c_sd,o_sd standard deviations for the moves on o and c
@@ -21,21 +22,46 @@
 #' ENSTestModel <- Peptides(ENSTestProtein)
 #' MCMS(ENSTestModel, var.model,
 #' 	prior_move_proportion = .02, c_sd = 0.05, o_sd = 0.05, o_k_scale = 1/100)
-#' \dontrun{
-#' # In a shell:
-#' R -e "library(MCMS); m <- MCMS(ENSTestModel, var.model, 10000000, 10000, 300000); \
-#' summary(as.data.frame(m))";
-#' }
 #' @export
 #' @useDynLib MCMS
-MCMS <- function(PeptidesModel, var.model, n, n.out, burn.in,
+MCMS <- function(Peptides, var.model, n = calcIterations(Peptides), n.out = 7E3, burn.in = 0.3, max.repeats = 20,
 				 c.prior.sd = sqrt(2), o.prior.shape1 = .5, o.prior.shape2 = .5,
 				 prior_move_proportion = .02, c_sd = 0.05,
 				 o_sd = 0.05, o_k_scale = 1/100, o_restrict = 1e-3,
 				 seed = round(runif(10, -.Machine$integer.max, .Machine$integer.max)),
 				 verbose = FALSE, cooling = TRUE) {
-	run_MCMC_Cpp(PeptidesModel, var.model, n, n.out, burn.in, c_prior_sd = c.prior.sd, o_prior_shape1 = o.prior.shape1,
-				 o_prior_shape2 = o.prior.shape2, o_restrict = o_restrict,
-				 prior_move_proportion = prior_move_proportion, c_sd = c_sd, o_sd = o_sd, o_k_scale = o_k_scale,
-				 seed = seed, verbose = verbose, cooling = cooling)
+
+	n.params <- nParams(Peptides)
+
+	if (n > 1E9) {
+		# Do 1E9 10 times with less output...
+		repeats <- min(ceiling(n / 1E9), 20) # max 10 repeats
+		n.out <- n.out / repeats # 1E4 - 30%
+		n <- 1E9
+	}
+	else {
+		repeats <- 1
+	}
+
+	mc.result <- sapply(seq_len(repeats), function(i) {
+		if (verbose) {
+			message(sprintf("Doing repeat %d/%d", i, repeats))
+		}
+
+		run_MCMC_Cpp(Peptides, var.model, n, n.out, burn.in * n, c_prior_sd = c.prior.sd, o_prior_shape1 = o.prior.shape1,
+					 o_prior_shape2 = o.prior.shape2, o_restrict = o_restrict,
+					 prior_move_proportion = prior_move_proportion, c_sd = c_sd, o_sd = o_sd, o_k_scale = o_k_scale,
+					 seed = seed, verbose = verbose, cooling = cooling)
+	}, simplify = FALSE)
+	mc.result <- do.call(rbind, mc.result)
+
+	if (! requireNamespace("coda", quietly = TRUE)) {
+		stop("coda package unavailable, effictive sample size not available")
+	}
+	else {
+		attr(mc.result, "effectiveSize") <-
+			coda::effectiveSize(coda::mcmc(mc.result[,seq_len(n.params)], start = (n * burn.in) + 1, end = n * repeats, thin = (n * repeats - (n * burn.in) - 1) / (n.out * repeats)))
+	}
+
+	return(mc.result)
 }
